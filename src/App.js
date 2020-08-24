ccd3d3612f64b2e9290d6162bdc196997606f880
agamkrbit <agamkrbit123@gmail.com>
2020-08-24 19:06:28 +0530

    auth done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import './App.scss';
 import Homepage from './pages/homepage/home_page.js';
 import ContactPage from './pages/contactpage/contact_page';
 
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
 function App() {
+  const loggedin = localStorage.getItem('token') || true;
   return (
     <div className="App">
-      <ContactPage/>
-      {/* <Homepage /> */}
+      <BrowserRouter>
+       <Switch>
+        <Route path="/home" exact component={Homepage} />
+        { loggedin && <Route path="/contacts" exact component={ContactPage} /> }
+        <Redirect to="/home" />
+       </Switch>
+      </BrowserRouter>
     </div>
   );
 }
